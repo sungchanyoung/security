510a120b01124caf22e53582e82f2eabe11286c1
SungChanYoung <zkzk7290@naver.com>
2025-06-11 23:55:13 +0900

    결제 주문 시스템

--- jwtWebServer/jwt/src/main/java/com/korit/jwt/filter/MyFilter.java
+++ jwtWebServer/jwt/src/main/java/com/korit/jwt/filter/MyFilter.java
@@ -1,6 +1,8 @@
 package com.korit.jwt.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
@@ -13,8 +15,14 @@ public class MyFilter  implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
+
+        HttpServletRequest req =  (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        String headerAuth = req.getHeader("Authoriztion");
+        System.out.println(headerAuth);
         System.out.println("filter1");
-        filterChain.doFilter(servletRequest,servletResponse);
+        filterChain.doFilter(req,res);
 
     }
 
